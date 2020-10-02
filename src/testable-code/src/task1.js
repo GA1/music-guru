9df80492d5ec382157cc2a834e83c9bdab788b94
GA1 <nvkga1@gmail.com>
2020-10-02 13:36:42 +0200

    feat: create chord mappings

--- src/testable-code/src/task1.js
+++ src/testable-code/src/task1.js
@@ -1,5 +1,5 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 const TRIAD_TYPE = Object.freeze({
@@ -9,7 +9,7 @@ const TRIAD_TYPE = Object.freeze({
   AUGMENTED: 4,
 })
 
-const toSolfegeNote = (note) => {
+const toSolfegeNote = note => {
   let result = note
   result = result.replace('C', 'do')
   result = result.replace('D', 're')
@@ -21,36 +21,43 @@ const toSolfegeNote = (note) => {
   return result
 }
 
-const toSolfegeNotes = (notes) => {
+const toSolfegeNotes = notes => {
   return notes.map(n => toSolfegeNote(n))
 }
 
-const C_major = 'C D E F G A B'
-const G_major = 'F♯ G A B C D E'
-const D_major = 'D E F♯ G A B C♯'
-const A_major = 'A B C D E F G'
-const E_major = 'E F♯ G♯ A B C♯ D♯'  // C♯ minor
-const F_sharp_major = 'F♯ G♯ A♯ B C♯ D♯ E♯' // D♯ minor
+const C_major = 'C D E F G A B'.split(' ')
+const G_major = 'F♯ G A B C D E'.split(' ')
+const D_major = 'D E F♯ G A B C♯'.split(' ')
+const A_major = 'A B C D E F G'.split(' ')
+const E_major = 'E F♯ G♯ A B C♯ D♯'.split(' ') // C♯ minor
+const F_sharp_major = 'F♯ G♯ A♯ B C♯ D♯ E♯'.split(' ') // D♯ minor
 
-const B_flat_major = 'B♭ C D E♭ F G A'
+const B_flat_major = 'B♭ C D E♭ F G A'.split(' ')
 
-const NUMBER_OF_CHORDS = 4
+const F_major = 'F G A B♭ C D E'.split(' ')
 
-const PAUSE = 15000
+const NUMBER_OF_CHORDS = 2
+
+const PAUSE_LENGTH = 1500
+const ALLOW_CONSECUTIVE_DUPLICATES = false
+
+const toMajorChords = scale => scale.map(note => note + 'maj')
+const toMinorChords = scale => scale.map(note => note + 'm')
+const to7Chords = scale => scale.map(note => note + '7')
+const toMinor7Chords = scale => scale.map(note => note + '7')
 
 // const notes = toSolfegeNotes(B_flat_major.split(' '))
-const notes = F_sharp_major.split(' ')
+const notes = [...toMajorChords(C_major), ...toMinorChords(C_major)]
 
-
-const convert = (letter) => {
+const convert = letter => {
   return {
-    'A': 'la',
-    'B': 'si',
-    'C': 'do',
-    'D': 're',
-    'E': 'mi',
-    'F': 'fa',
-    'G': 'sol',
+    A: 'la',
+    B: 'si',
+    C: 'do',
+    D: 're',
+    E: 'mi',
+    F: 'fa',
+    G: 'sol',
   }[letter]
 }
 
@@ -62,10 +69,12 @@ const generateAllPossibleSequences = length => {
       for (let i = 0; i < notes.length; i++) {
         const note = notes[i]
         const N = sequence.length
-        if (0 < N && sequence[N - 1] === note) {
-          return
-        } else {
-          generateAllPossibleSequencesRecursively([...sequence, note], notes, acc)
+        if (0 === N || ((0 < N && sequence[N - 1] !== note) || ALLOW_CONSECUTIVE_DUPLICATES)) {
+          generateAllPossibleSequencesRecursively(
+            [...sequence, note],
+            notes,
+            acc
+          )
         }
       }
     }
@@ -76,31 +85,29 @@ const generateAllPossibleSequences = length => {
 }
 
 let itemsToDraw = []
-
-function generateTriadSeries(lengths) {
+function generateChordProgression(lengths) {
   for (let i = 0; i < lengths.length; i++) {
     const length = lengths[i]
-    const allPossibleSequencesOfLength = generateAllPossibleSequences(length);
+    const allPossibleSequencesOfLength = generateAllPossibleSequences(length)
     itemsToDraw = [...itemsToDraw, ...allPossibleSequencesOfLength]
   }
 }
 
-generateTriadSeries([NUMBER_OF_CHORDS])
+generateChordProgression([NUMBER_OF_CHORDS])
 
+console.log(itemsToDraw)
+console.log(itemsToDraw.length)
 const f = async () => {
   while (true) {
-    const drawnSequence = itemsToDraw[Math.floor(Math.random() * itemsToDraw.length)]
+    const drawnSequence =
+      itemsToDraw[Math.floor(Math.random() * itemsToDraw.length)]
     console.log('\n\n\n\n\n\n')
     // console.log(drawnSequence.split('').map(l => convert(l)).join(' '))
 
     console.log(drawnSequence.join(' '))
     console.log('\n\n\n\n\n\n')
-    await sleep(PAUSE)
+    await sleep(PAUSE_LENGTH)
   }
 }
 
 f().then(() => {})
-
-
-
-
